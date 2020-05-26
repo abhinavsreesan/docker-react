19aff882aac1f1646faa4c26429bd20c2362e784
abhinavsreesan <abhinavsreesancp@gmail.com>
2020-05-26 22:57:05 +0530

    Changes the tex

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          what's poppin!
+          Abhinav Sreesan's first React APP
         </p>
         <a
           className="App-link"
